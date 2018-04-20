e6a42a0083a6cbeacc1d07961bfd20c56d7d649d
Markus Ullrich <mullrich@hszg.de>
2018-04-20 11:19:13 +0200

    Initial import (Spring Initializer)

---
+++ src/main/java/org/bissis/CloudRdeBaseApplication.java
@@ -0,0 +1,12 @@
+package org.bissis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudRdeBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudRdeBaseApplication.class, args);
+	}
+}
